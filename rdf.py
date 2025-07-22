874abace35198d609e89dc008bc36e14d268603c
Roxanne Berthin <roxanne.berthin1@gmail.com>
2025-07-22 12:49:04 +0200

    création du module bd_stuff

--- rdf.py
+++ rdf.py
@@ -1,15 +1,5 @@
 import numpy as np
-
-def read_one_frame(f, n_atoms):
-    """Lit une frame dans le fichier .xyz et renvoie les atomes"""
-    f.readline()  # Skip comment line
-    data = []
-    for _ in range(n_atoms):
-        parts = f.readline().split()
-        data.append((int(parts[0]), float(parts[1]), float(parts[2]), float(parts[3])))
-    return np.array(data)
-
-
+from bd_stuff import read_one_frame
 
 trajfile = "traj_rho_0.2_eps_2.5.xyz"
 box = 20.32
