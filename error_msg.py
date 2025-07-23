4afabf9fa28f602c290326d3ceeebc6a0800718a
Roxanne Berthin <roxanne.berthin1@gmail.com>
2025-07-23 17:41:29 +0200

    add error message on traj and compute

---
+++ error_msg.py
@@ -0,0 +1,7 @@
+
+
+def error_traj():
+    print('The trajectory name should end with xyz..\n')
+
+def error_func(compute):
+    print('The function {} do not exist. Please choose another one\n'.format(compute))
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # librairie à importer
 import argparse
 from rdf import rdf_computation
+import error_msg
 
 # Welcome menu
-print('-------------------------------------------')
-print('*******************************************')
-print('------------- !! BD  TOOLS !! -------------')
-print('*******************************************')
-print('-------------------------------------------')
+print('-------------------------------------------------------')
+print('*******************************************************')
+print('------------------- !! BD  TOOLS !! -------------------')
+print('*******************************************************')
+print('-------------------------------------------------------')
 
 # Arguments 
 parser = argparse.ArgumentParser()
@@ -15,29 +16,42 @@ parser.add_argument('-i', '--input', type=str,
                     help = 'input file containing information about the system')
 args = parser.parse_args()
 
+bool_traj = True
+bool_func = True
+func_list = ['rdf']
 
 if args.input:
     inputfile = open(args.input, 'r')
     trajfile = inputfile.readline().rstrip()
-    print(trajfile)
     box = float(inputfile.readline())
     compute = inputfile.readline().rstrip()
-    print(compute)
 else:
     print('No input file specified ...\n')
-    print('-------------------------------------------')
-    print('----------- GENERAL INFORMATION -----------')
-    print('-------------------------------------------')
-    trajfile = input('Name of the trajectory file?\n')
+    print('-------------------------------------------------------')
+    print('----------------- GENERAL INFORMATION -----------------')
+    print('-------------------------------------------------------')
+
+    while bool_traj:
+        trajfile = input('Name of the trajectory file?\n')
+        if not trajfile.endswith('.xyz'):
+            error_msg.error_traj()
+        else:
+            bool_traj = False
+    
     box = float(input('Length of the box?\n'))
-    print('-------------------------------------------')
-    print('--------------- COMPUTATION ---------------')
-    print('-------------------------------------------')
+    print('-------------------------------------------------------')
+    print('--------------------- COMPUTATION ---------------------')
+    print('-------------------------------------------------------')
     print('List of functions:')
     print('------------------')
     print('-- Radial distribution function (rdf)')
-    print('-------------------------------------------')
-    compute = input('Which function do you want to compute?\n')
+    print('-------------------------------------------------------')
+    while bool_func:
+        compute = input('Which function do you want to compute?\n')
+        if compute not in func_list:
+            error_msg.error_func(compute)
+        else:
+            bool_func = False
 
 match compute:
     case "rdf":
@@ -59,4 +73,3 @@ match compute:
         # call rdf function
         rdf_computation(trajfile, box, nbins, start_step,
                         end_step, species_1, species_2)
-
